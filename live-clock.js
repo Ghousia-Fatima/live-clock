51c2b821f67ed503164977e1ddf0450186ba20e4
Ghousia-Fatima <120248904+Ghousia-Fatima@users.noreply.github.com>
2023-05-28 20:49:08 +0530

    Test Task - 2: Live clock

---
+++ live-clock.js
@@ -0,0 +1,22 @@
+// var span = document.getElementById('span');
+
+// function time() {
+//   var d = new Date();
+//   var s = d.getSeconds();
+//   var m = d.getMinutes();
+//   var h = d.getHours();
+//   span.textContent = 
+//     ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+// }
+
+// setInterval(time, 1000);
+
+window.onload = displayClock();
+function displayClock(){
+  var display = new Date().toLocaleTimeString();
+  document.body.innerHTML = display;
+  setTimeout(displayClock, 1000); 
+}
+
+
+
